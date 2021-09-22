bf2f2f1c33794ffd7c8853048dc2cf1b022e3732
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-22 19:48:54 -0400

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,8 +1,8 @@
 public class Matrix{
     public static void main(String[] args){
         Matrix matrix = new Matrix;
-        double[] dot_x() = new double[] {1,1,1};
-        double[] dot_y() = new double[] {1,2,3};
+        double[] dot_x = new double[] {1,1,1};
+        double[] dot_y = new double[] {1,2,3};
         double dot_Product - new matrix.dot(dot_x, dot_y); 
         System.out.println("The dot product of matrix x and y is " + dot_Product);
         
