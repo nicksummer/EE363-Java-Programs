4aa535a125d65e7bd5ffcd152a913fd8b080d6b4
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-22 19:38:47 -0400

    Added new matrix.java

---
+++ Matrix.java
@@ -0,0 +1,18 @@
+public double dot(double[]x, double[] y)(
+        int minimum = x.length > b.length ? b.length:a.length;
+        double out = 0;
+        for(int i =0; i< minimum; i++)
+            output += a[i] * b[i];
+)
+return output;
+
+public class Matrix{
+    public static void main(String[] args)(
+        Matrix matrix = new Matrix;
+        double[] dot_x() = new double[] {1,1,1};
+        double[] dot_y() = new double[] {1,2,3};
+        double dot_Product - new matrix.dot(dot_x,dot,dot_y); 
+        System.out.println("The dot product of matrix x and y is " + dot_Product);
+     )
+}
+
