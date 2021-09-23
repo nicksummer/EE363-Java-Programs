1d1abe0b85e660f29af9212be1f57479ab837e29
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-22 20:15:05 -0400

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,22 +1,27 @@
 public class Matrix{
+
+    private Matrix() { }
+
+    public double dot_product(double[] x, double[] y){
+        int minimum = x.length > y.length ? x.length:y.length;
+        double outp = 0;
+        for(int i =0; i< minimum; i++)
+            outp += x[i] * y[i];
+
+        return outp;
+    }
+
     public static void main(String[] args){
-        Matrix matrix = new Matrix();
-        double[] dot_x = new double[] {1,1,1};
-        double[] dot_y = new double[] {1,2,3};
-        double dot_Product = new matrix.dot_product(dot_x, dot_y); 
+      
+
+        //Matrix matrix = new matrix();
+        double[] x = new double[] {1,1,1};
+        double[] y = new double[] {1,2,3};
+        double dot_Product = new dot_product(x, y); 
         System.out.println("The dot product of matrix x and y is " + dot_Product);
         
     }
 
-    public double dot_product(double[]x, double[] y){
-        int minimum = x.length > y.length ? x.length:y.length;
-        double out = 0;
-        for(int i =0; i< minimum; i++)
-            out += x[i] * y[i];
-
-        return 1;
-    }
-   
-
+    
 }
 
