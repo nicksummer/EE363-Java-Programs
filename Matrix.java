bfecd7b2bdb8341e5b71ff5fb15742b2bf3f2209
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-22 20:44:29 -0400

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -2,6 +2,15 @@ public class Matrix{
 
     private Matrix() { }
 
+    public static double transpose(double[][]x ){
+        double[][] transpose = new double[][] { };
+            
+            for(i = 0; i < x.length; i++)
+                for(j = 0; j < x[0].length; i++)
+                    System.out.println(x + " " + y );
+
+    }
+
     public static double dot_product(double[] x, double[] y){
         int minimum = x.length < y.length ? x.length:y.length;
 
@@ -13,13 +22,14 @@ public class Matrix{
     }
 
     public static void main(String[] args){
-      
-
-        //Matrix matrix = new matrix();
-        double[] x = new double[] {1,1,1};
-        double[] y = new double[] {1,2,3};
-        double dot_Product = Matrix.dot_product(x, y); 
-        System.out.println("The dot product of matrix x and y is " + dot_Product);
+        //transpose
+        double[][] a = new double[][] {{1,2,3}, {4,5,6}};
+        transpose(a);
+        //dot product
+       // double[] x = new double[] {1,1,1};
+       // double[] y = new double[] {1,2,3};
+       // double dot_Product = Matrix.dot_product(x, y); 
+       // System.out.println("The dot product of matrix x and y is " + dot_Product);
         
     }
 
