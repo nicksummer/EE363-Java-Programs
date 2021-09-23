93e10337944f20982b233e089d867f0c15ac9f0a
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-23 18:47:52 -0400

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -8,7 +8,7 @@ public class Matrix{
 
             for(int i = 0; i < transpose_b.length; i++)
                 for(int j = 0; j < transpose_b[1].length; j++)
-                    matrix_Mult[i][j] = Matrix.dot_product(a[i], Matrix.transpose.b[i].length);
+                    matrix_Mult[i][j] = Matrix.dot_product(a[i], Matrix.transpose_b[i].length);
 
             return matrix_Mult;
 
