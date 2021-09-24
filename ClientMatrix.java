910aab51a3d3a6a2a3e5b94b5794817aaedcce7e
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-23 21:23:50 -0400

    Update ClientMatrix.java

--- ClientMatrix.java
+++ ClientMatrix.java
@@ -1,52 +1,48 @@
+//Nicholas Summerville
+//EE363 Professor Hussian
+//Main client to illustrate how the various different functions in the Matrix class works
 class ClientMatrix{
 
     public static void main(String[] args){
         //matrix-matrix multiplication 
-        double[][]   a = new double[][] {{1,2,3}, {2,3,4}};
-        double[][]   b = new double[][] {{1,2}, {3,4}, {5, 6}};
-        Matrix.print_matrix(a);
+        double[][]   matrixMat_a = new double[][] {{1,2,3}, {2,3,4}};
+        double[][]   matrixMat_b = new double[][] {{1,2}, {3,4}, {5, 6}};
+        Matrix.print_matrix(matrixMat_a);
         System.out.println("----------------");
-        Matrix.print_matrix(b);
+        Matrix.print_matrix(matrixMat_b);
         System.out.println();
         System.out.println();
-        Matrix.print_matrix(Matrix.matrix_matrix(a,b));
+        Matrix.print_matrix(Matrix.matrix_matrix(matrixMat_a, matrixMat_b));
 
-
-    /*
         //vector-matrix multiplication 
-        double[][] a = new double[][] {{1,2,3}, {4,5,6}};
-        double[]   x = new double[] {1,2,3};
-        Matrix.print_vector(x);
-        Matrix.print_matrix(a);
+        double[][] VecMat_a = new double[][] {{1,2,3}, {4,5,6}};
+        double[]   VecMat_x = new double[] {1,2,3};
+        Matrix.print_vector(VecMat_x);
+        Matrix.print_matrix(VecMat_a);
         System.out.println();
         System.out.println();
-        Matrix.print_vector(Matrix.vector_matrix(x, a));
-    */
-
-    /*
+        Matrix.print_vector(Matrix.vector_matrix(VecMat_x, VecMat_a));
+    
         // Matrix-Vector multiplication 
-        double[][] a = new double[][] {{1,2,3}, {4,5,6}};
-        double[]   x = new double[] {1,2,3};
-        Matrix.print_vector(x);
-        Matrix.print_matrix(a);
+        double[][]  MatVec_a = new double[][] {{1,2,3}, {4,5,6}};
+        double[]    MatVec_x = new double[] {1,2,3};
+        Matrix.print_vector(MatVec_x);
+        Matrix.print_matrix(MatVec_a);
         System.out.println();
         System.out.println();
-        Matrix.print_vector(Matrix.matrix_vector(a, x));
-        */
-
-
-
-    /*
+        Matrix.print_vector(Matrix.matrix_vector(MatVec_a, MatVec_x));
+        
         //transpose
-        double[][] a = new double[][] {{1,2,3}, {4,5,6}};
-        Matrix.print(a);
-        double[][] t = Matrix.transpose(a);
+        double[][] trans_a = new double[][] {{1,2,3}, {4,5,6}};
+        Matrix.print(trans_a);
+        double[][] t = Matrix.transpose(trans_a);
+        System.out.println("The transpose of the matrix " + trans_a + ". ");
         Matrix.print(t);
-    */
+    
         //dot product
-     // double[] x = new double[] {1,1,1};
-    // double[] y = new double[] {1,2,3};
-    // double dot_Product = Matrix.dot_product(x, y); 
-    // System.out.println("The dot product of matrix x and y is " + dot_Product);
+        double[] dot_x = new double[] {1,1,1};
+        double[] dot_y = new double[] {1,2,3};
+        double dot_Product = Matrix.dot_product(dot_x, dot_y); 
+        System.out.println("The dot product of matrix x and y is " + dot_Product);
     }
 }
