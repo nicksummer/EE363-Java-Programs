49c64902746185aa3bb408e15e596658a9063b6c
Nick <52978388+nicksummer@users.noreply.github.com>
2021-09-23 21:00:42 -0400

    Update ClientMatrix.java

--- ClientMatrix.java
+++ ClientMatrix.java
@@ -1,6 +1,6 @@
 import Matrix.java;
 
-public static Matrix main(String[] args){
+public static void main(String[] args){
     //matrix-matrix multiplication 
     double[][]   a = new double[][] {{1,2,3}, {2,3,4}};
     double[][]   b = new double[][] {{1,2}, {3,4}, {5, 6}};
