32089e3521f35c3d46664f4f4d514a7069ea47d4
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-13 16:46:14 -0400

    fix

---
+++ Homework2/Rational.java
@@ -0,0 +1,22 @@
+public class Rational{
+    
+    public static Rational Rational(int numerator, int denominator){
+        
+        if(denominator == 0)
+            throw new IllegalArguementException("The denominator cannot be zero");
+        
+            if (numerator == 0){
+                this.numerator = 0;
+                this.denominator = 1;
+            }
+            else {
+                this.numerator = numerator;
+                this.denominator = denominator;
+            }
+    }
+
+    public String toString(){
+        return numerator + "/" + denominator;
+    }
+
+}
