292ce6348ebb95293a7245dc53ad99afd63a0211
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-13 23:01:05 -0400

    yes

--- Homework2/hw2/p1/Rational.java
+++ Homework2/hw2/p1/Rational.java
@@ -1,21 +1,33 @@
 public class Rational{
-    
-    public static int Rational(int numerator, int denominator){
-        
-        if(denominator == 0)
-            throw new IllegalArguementException("The denominator cannot be zero");
-        
-            if (numerator == 0){
-                this.numerator = 0;
-                this.denominator = 1;
-            }
-            else {
-                this.numerator = numerator;
-                this.denominator = denominator;
-            }
+    private final numerator;
+    private final denominator;
+
+    public Rational(int numerator, int denominator){
+        this(5,4);
     }
+    // public Rational(int numerator, int denominator){
+
+    //     int gcd = gcd(numerator, denominator)
+        
+    //     if(denominator == 0)
+    //         throw new IllegalArguementException("The denominator cannot be zero");
+        
+    //         if (numerator == 0){
+    //             this.numerator = -numerator/gcd;
+    //             this.denominator = -denominator/gcd;
+    //         }
+    //         else {
+    //             this.numerator = numerator;
+    //             this.denominator = denominator;
+    //         }
+    // }
 
     public String toString(){
+        if(denominator == 1)
+            return numerator;
+        else if (numerator == 0)
+            return 0;
+        else
         return numerator + "/" + denominator;
     }
 
