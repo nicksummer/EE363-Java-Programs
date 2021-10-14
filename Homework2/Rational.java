07d02d004fe28c1c8f3d3dcd7f6c58100f0a0bcc
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-13 22:52:37 -0400

    yep

--- Homework2/Rational.java
+++ Homework2/Rational.java
@@ -1,6 +1,9 @@
 public class Rational{
-    
+    private final numerator;
+    private final denominator;
     public static Rational Rational(int numerator, int denominator){
+
+        int gcd = gcd(numerator, denominator)
         
         if(denominator == 0)
             throw new IllegalArguementException("The denominator cannot be zero");
@@ -16,7 +19,12 @@ public class Rational{
     }
 
     public String toString(){
-        return numerator + "/" + denominator;
+        if(denominator == 1)
+            return numerator;
+        else if (numerator == 0)
+            return 0;
+        else
+        return numer + "/" + denominator;
     }
 
 }
