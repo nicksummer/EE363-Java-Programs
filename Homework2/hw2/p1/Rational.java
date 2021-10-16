8d53c64133b2282b163f372495bd67d84c572392
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-16 15:44:57 -0400

    changes

--- Homework2/hw2/p1/Rational.java
+++ Homework2/hw2/p1/Rational.java
@@ -38,12 +38,11 @@ public class Rational{
     }
 
     public static void main(String[] args){
-        Rational test = new Rational(5,4);
-        StdOut.println("\n" + test);
-		// Rational r1 = new Rational(5, 6);
-		// Rational r2 = new Rational(3, 4);
-		// StdOut.println("r1: " + r1);
-		// StdOut.println("r2: " + r2);
+       
+		Rational r1 = new Rational(5, 6);
+		Rational r2 = new Rational(3, 4);
+		StdOut.println("r1: " + r1);
+		StdOut.println("r2: " + r2);
 
 		//Rational rsum = r1.plus(r2);
 		//StdOut.println("r1 + r2: " + rsum);
