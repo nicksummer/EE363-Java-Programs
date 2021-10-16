ffc8e7eaf572242fff219ab2fcd5f3fd5f911ff6
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-16 17:01:09 -0400

    changes

--- Homework2/hw2/p1/Rational.java
+++ Homework2/hw2/p1/Rational.java
@@ -8,7 +8,6 @@ public class Rational{
     private long denom;
 
     public Rational minus(Rational that){
-        that = new Rational(-num, denom);
         long GCDNum = gcd(this.num, that.num);
         long GCDDenom = gcd(this.denom, that.denom);
 
