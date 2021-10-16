59257768355f93601520c96ccf6fcece8c1c1c24
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-16 17:50:08 -0400

    changes

--- Homework2/hw2/p1/Rational.java
+++ Homework2/hw2/p1/Rational.java
@@ -95,53 +95,53 @@ public class Rational{
 
     public static void main(String[] args){
 
-		// Rational r1 = new Rational(5, 6);
-		// Rational r2 = new Rational(3, 4);
-		// StdOut.println("r1: " + r1);
-		// StdOut.println("r2: " + r2);
+		Rational r1 = new Rational(5, 6);
+		Rational r2 = new Rational(3, 4);
+		StdOut.println("r1: " + r1);
+		StdOut.println("r2: " + r2);
 
-		// Rational rsum = r1.plus(r2);
-		// StdOut.println("r1 + r2: " + rsum);
+		Rational rsum = r1.plus(r2);
+		StdOut.println("r1 + r2: " + rsum);
 		
-		// Rational rdiff = r1.minus(r2);
-		// StdOut.println("r1 - r2: " + rdiff);
+		Rational rdiff = r1.minus(r2);
+		StdOut.println("r1 - r2: " + rdiff);
 		
-		// Rational rprod = r1.times(r2);
-		// StdOut.println("r1 * r2: " + rprod);
+		Rational rprod = r1.times(r2);
+		StdOut.println("r1 * r2: " + rprod);
 		
-		// Rational rquot = r1.dividedBy(r2);
-		// StdOut.println("r1 / r2: " + rquot);
+		Rational rquot = r1.dividedBy(r2);
+		StdOut.println("r1 / r2: " + rquot);
 
 
-		// System.out.println();
+		System.out.println();
 
-		// StdOut.println("[Rational cr1 = r1;]");
-		// Rational cr1 = r1;
-		// StdOut.println("cr1: " + cr1);
+		StdOut.println("[Rational cr1 = r1;]");
+		Rational cr1 = r1;
+		StdOut.println("cr1: " + cr1);
 		
 
-		// Rational n5 = new Rational(7, 8);
-		// StdOut.println("[Rational n5 = new Rational(7, 8);]");
-		// StdOut.println("r1: " + r1);
-		// StdOut.println("n5: " + n5);		
-		// StdOut.println("r1 equals cr1: " + r1.equals(cr1));
-		// StdOut.println("r1 equals n5: " + r1.equals(n5));
-		// System.out.println();
+		Rational n5 = new Rational(7, 8);
+		StdOut.println("[Rational n5 = new Rational(7, 8);]");
+		StdOut.println("r1: " + r1);
+		StdOut.println("n5: " + n5);		
+		StdOut.println("r1 equals cr1: " + r1.equals(cr1));
+		StdOut.println("r1 equals n5: " + r1.equals(n5));
+		System.out.println();
 
-		// //another equals test (test contents, not just references)		
-		// Rational cr2 = new Rational(5, 6);
-		// StdOut.println("[Rational cr2 = new Rational(5, 6);]");
-		// StdOut.println("r1: " + r1);
-		// StdOut.println("cr2: " + cr2);		
-		// StdOut.println("r1 equals cr2: " + r1.equals(cr2));
-		// System.out.println();
+		//another equals test (test contents, not just references)		
+		Rational cr2 = new Rational(5, 6);
+		StdOut.println("[Rational cr2 = new Rational(5, 6);]");
+		StdOut.println("r1: " + r1);
+		StdOut.println("cr2: " + cr2);		
+		StdOut.println("r1 equals cr2: " + r1.equals(cr2));
+		System.out.println();
 				
 
 
-		// Rational r2plusn5 = r2.plus(n5);
-		// Rational res_r2plusn5 = new Rational(15, 8);
-		// StdOut.println("r2 + n5: "+ r2plusn5); 
-		// StdOut.println("res_r2plusn5: " + r2plusn5);
+		Rational r2plusn5 = r2.plus(n5);
+		Rational res_r2plusn5 = new Rational(15, 8);
+		StdOut.println("r2 + n5: "+ r2plusn5); 
+		StdOut.println("res_r2plusn5: " + r2plusn5);
 		
 		//use VM flag '-ea' to cause overflow exception
 		StdOut.println("\nOverflow test:");
