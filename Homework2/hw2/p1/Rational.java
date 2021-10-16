f7d5f026677378ffaec42a856d9cfeb9f4a6e587
Nick <52978388+nicksummer@users.noreply.github.com>
2021-10-16 19:34:51 -0400

    changes

--- Homework2/hw2/p1/Rational.java
+++ Homework2/hw2/p1/Rational.java
@@ -100,7 +100,7 @@ final class Rational{
     public static void main(String[] args){
 
 		Rational r1 = new Rational(-5, -6);
-		Rational r2 = new Rational(-3, 4);
+		Rational r2 = new Rational(3, -4);
 		StdOut.println("r1: " + r1);
 		StdOut.println("r2: " + r2);
 
