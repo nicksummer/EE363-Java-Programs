0f7969d41354542005fe71ec9f0d8af5a719c963
Nick <52978388+nicksummer@users.noreply.github.com>
2021-12-03 12:59:04 -0500

    s

---
+++ files.hw4/multiThreading/RCompute.java
@@ -0,0 +1,14 @@
+package hw4.p1; 
+
+public class RCompute implements runnable {
+int left = 40
+int right = 250
+    public RCompute averageOfSquares(left, right){
+        double sum = right * (right +1)/2 - (left -1) * left / 2;
+        double counter = right - left + 1
+        
+        average = sqrt(sum) / counter; 
+    public void run(){
+        
+    }
+}
---
+++ files.hw4/unitTesting/EmptyException.java
@@ -0,0 +1,14 @@
+package hw4.p2;
+
+public class EmptyException extends RuntimeException {
+    public EmptyException() {
+	super();
+    }
+
+
+    public EmptyException(String s) {
+	super(s);
+    }
+
+}
+
