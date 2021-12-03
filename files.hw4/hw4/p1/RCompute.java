87aa4294166a4c2dc605a0f7d904a0845682d62c
Nick <52978388+nicksummer@users.noreply.github.com>
2021-12-03 14:13:49 -0500

    s

--- files.hw4/hw4/p1/RCompute.java
+++ files.hw4/hw4/p1/RCompute.java
@@ -1,22 +1,24 @@
 package hw4.p1;
 public class RCompute implements Runnable {
-    double average = 0;
-
+ 
+    private int left;
+    private int right;
     public RCompute(int left, int right) {
-        double total = 0;
-        int counter = 0;
-        for (int i = left; i < right; i++) {
-            if (i == left) {
-                counter = counter + 1;
-                total = sqrt(total + i);
-            } else {
-                counter = counter + 1 ;
-                total = sqrt(total + (i + 1));
-            }
-        }
-        average = total / counter;
+       this.left = left;
+       this.right = right;
     }
 
     public void run() {
+        double total = 0;
+        int counter = 0;
+        for (int i = left; i <= right; i++) {
+          
+            
+          counter = counter + 1 ;
+          total += Math.sqrt(i);
+        
+        }
+        average = total / counter;
+        System.out.printf("average of square roots is: %.2f %n", average);
     }
 }
